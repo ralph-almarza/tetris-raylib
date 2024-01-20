d2e9c0d75976e1467caeab090cc536d5e64b2f3b
Ralph Aaron Almarza <almarza.ralph.aaron@gmail.com>
2024-01-20 20:30:42 +0800

    Grid Added Print() function
    Others
    Changed numCols to numColumns

--- grid.cpp
+++ grid.cpp
@@ -1,8 +1,21 @@
 #include "grid.h"
 
+#include <iostream>
+
 Grid::Grid() // Constructor
 {
 	numRows = { 20 };
-	numCols = { 10 };
+	numColumns = { 10 };
 	cellSize = { 30 };
+}
+
+void Grid::Print()
+{
+	for (int row{ 0 }; row < numRows; ++row)
+	{
+		for (int column{ 0 }; column < numColumns; ++column)
+		{
+			std::cout << grid[row][column] << " ";
+		}
+	}
 }
--- grid.h
+++ grid.h
@@ -5,9 +5,10 @@ class Grid
 public:
 	Grid();
 	int grid[20][10] { 0 }; // array of 20 rows and 10 columns
+	void Print();			// outputs the grid in console
 
 private:
-	int numRows{}; // height of game grid 
-	int numCols{}; // width of game grid 
+	int numRows{};	// height of game grid 
+	int numColumns{};	// width of game grid 
 	int cellSize{}; // # of pixels denoting one cell in game grid 
 };
